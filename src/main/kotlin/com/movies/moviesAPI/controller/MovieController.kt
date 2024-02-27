b3efde4b1bc02adf4c301b1bae8fa032d0a7c0d2
E21B036Z <alexandre.carol@etu.univ-nantes.fr>
2024-02-27 19:47:20 +0100

    Update Movie

--- src/main/kotlin/com/movies/moviesAPI/controller/MovieController.kt
+++ src/main/kotlin/com/movies/moviesAPI/controller/MovieController.kt
@@ -5,8 +5,9 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.time.Month
 
-data class Movie(val Id : Int, val name : String, val releaseDate : String)
+data class Movie(val Id : Int, val name : String, val month: Int, val year : Int)
 
 @RestController
 @Validated
--- src/main/kotlin/com/movies/moviesAPI/service/MovieService.kt
+++ src/main/kotlin/com/movies/moviesAPI/service/MovieService.kt
@@ -7,25 +7,25 @@ import org.springframework.stereotype.Service
 class MovieService : IMovieService {
     override fun getMovies(): List<Movie> {
         return listOf(
-                Movie(1, "Titanic", "2024-02-27"),
-                Movie(2, "Moi, moche et méchant", "2021-12-05"),
-                Movie(3, "Ducobu", "2003-02-11"),
-                Movie(4, "Matrix", "2006-08-27"),
-                Movie(5, "Avengers", "2020-11-01"),
-                Movie(6, "Lucas et Alexandre", "2024-01-31"),
-                Movie(7, "Tous au five", "2012-06-09"),
+                Movie(1, "Titanic", 2, 1950),
+                Movie(2, "Moi, moche et méchant", 6, 2010),
+                Movie(3, "Ducobu", 1, 2000),
+                Movie(4, "Matrix", 8, 1997),
+                Movie(5, "Avengers", 12, 2018),
+                Movie(6, "Lucas et Alexandre", 2, 2023),
+                Movie(7, "Tous au five", 7, 2024),
         )
     }
 
     override fun getMovieById(id : Int): Movie? {
         val movies : List<Movie> = listOf(
-                Movie(1, "Titanic", "2024-02-27"),
-                Movie(2, "Moi, moche et méchant", "2021-12-05"),
-                Movie(3, "Ducobu", "2003-02-11"),
-                Movie(4, "Matrix", "2006-08-27"),
-                Movie(5, "Avengers", "2020-11-01"),
-                Movie(6, "Lucas et Alexandre", "2024-01-31"),
-                Movie(7, "Tous au five", "2012-06-09"),
+                Movie(1, "Titanic", 2, 1950),
+                Movie(2, "Moi, moche et méchant", 6, 2010),
+                Movie(3, "Ducobu", 1, 2000),
+                Movie(4, "Matrix", 8, 1997),
+                Movie(5, "Avengers", 12, 2018),
+                Movie(6, "Lucas et Alexandre", 2, 2023),
+                Movie(7, "Tous au five", 7, 2024),
         )
 
         movies.forEach { movie ->
